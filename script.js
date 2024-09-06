f68a2c35caef8e672c4c8c11b560f6d516e42aff
TheNameisNavin <navinkumarspm@outlook.com>
2024-09-06 12:30:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.querySelectorAll('.hover-sound').forEach(link => {
-    link.addEventListener('mouseover', () => {
-        const sound = document.getElementById('hover-sound');
-        sound.play();
-    });
-});
